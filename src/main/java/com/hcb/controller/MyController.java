813fd799fb32463a3f8909a791077dd79a4b4318
hcb <huang_chaobing@163.com>
2018-05-04 07:17:30 +0800

    add controller

---
+++ src/main/java/com/hcb/controller/MyController.java
@@ -0,0 +1,12 @@
+package com.hcb.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+	@RequestMapping("/hello")
+public String test() {
+	return "good";
+}
+}
